d0db7699b54129b4740ba866e691c12c271e752b
BrieFishyW <jrutherford@mail.weber.edu>
2023-12-09 09:46:37 -0700

    Entry list page formatted nicely

--- diaryapp/views.py
+++ diaryapp/views.py
@@ -9,7 +9,7 @@ def listpage (request):
     entry_title = request.GET.get('entry_title')
     if entry_title != '' and entry_title is not None:
         entry_list = entry_list.filter(entry_title__icontains=entry_title)
-    paginator = Paginator(entry_list, 5)
+    paginator = Paginator(entry_list, 10)
     page = request.GET.get('page')
     entry_list = paginator.get_page(page)
     return render(request, 'listpage.html', {'entry_list': entry_list})
